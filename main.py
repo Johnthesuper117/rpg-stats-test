e922563e76b77e9dc6c85aeb58cb89c0df12267e
Johnthesuper117 <195603431+Johnthesuper117@users.noreply.github.com>
2025-04-07 10:12:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,17 +10,10 @@ class Player:
     self.attack = attack
     self.defence = defence
     
-  def check(self):
-    if int(self.health) + int(self.attack) + int(self.defence) > 300:
-      result = "REDO"
-    else:
-      result = "CONTINUE"
-    print(f"Player name: {player.name}\nPlayer health: {player.health}\nPlayer attack: {player.attack}\nPlayer defence: {player.defence}")
-    return result
-
 print("You have a max of 300 stat points to use total\n")
 player = Player(input("Enter username: "), input("Enter health stat: "), input("Enter attack stat: "), input("Enter defence stat: "))
+print(f"Player name: {player.name}\nPlayer health: {player.health}\nPlayer attack: {player.attack}\nPlayer defence: {player.defence}")
 if int(player.health) + int(player.attack) + int(player.defence) > 300:
   print(f"Max stat points is 300, and you added {abs(int(player.health) + int(player.attack) + int(player.defence)) - 300} too many, please try agian\n")
   player.change(input("Enter health stat: "), input("Enter attack stat: "), input("Enter defence stat: "))
-
+print(f"Player name: {player.name}\nPlayer health: {player.health}\nPlayer attack: {player.attack}\nPlayer defence: {player.defence}")
